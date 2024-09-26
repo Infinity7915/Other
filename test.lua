03c8ab54f86a1e472f65dddafd892cd488a8a0ad
Infinity7915 <dustinmatt360@gmail.com>
2024-09-26 10:36:50 -0500

    Update test.lua

--- test.lua
+++ test.lua
@@ -1,1 +1,10 @@
 
+return {
+    {7318019243},  -- UserId only
+    {1502149925},
+    {1955916849},  -- UserId only
+    {1762542484},
+    {6099366721},
+    {4324069918},  -- UserId only
+    -- Add more entries as needed
+}
